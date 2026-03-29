73ca550c715f3dd139a58e2a0e7971e72c3d3484
Priya_Shukla16 <priyashukla160106@gmail.com>
2026-03-29 22:50:04 +0530

    Update Sidebar.jsx

--- frontend/src/layout/Sidebar.jsx
+++ frontend/src/layout/Sidebar.jsx
@@ -4,7 +4,7 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <h2>CarbonTrack</h2>
-      
+
       <nav>
         <Link to="/app/home">Home</Link>
         <Link to="/app/data-entry">Data Entry</Link>
