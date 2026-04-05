1aa2a517715c623806f50356ec643cf1da5c80ff
Neha <nehasingha2006@gmail.com>
2026-04-05 15:23:54 +0530

    Fixed DB connection error

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => (
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-    
-  } catch (err) {
-    console.error(err);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
