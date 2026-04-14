36f7a6d8c9bfdffb91cf868150ba6bead51ab920
Priya_Shukla16 <priyashukla160106@gmail.com>
2026-04-14 21:24:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+
 require("dotenv").config();
+
 const connectDB = require("./config/db");
 
 const carbonRoutes = require("./routes/carbonRoutes");
